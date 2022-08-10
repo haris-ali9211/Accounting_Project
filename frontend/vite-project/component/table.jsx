f34ce1552b993ba4121b438036b49f5a471b2542
Haris Ali <haris.aali9211@gmail.com>
2022-08-11 02:32:04 +0500

    general journal done

---
+++ frontend/vite-project/component/table.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import Table from 'react-bootstrap/Table';
+
+function DarkExample() {
+
+  const [count, setCount] = useState(0)
+  const [apiData, setApiData] = useState([]);
+  console.log("🚀 ~ file: App.jsx ~ line 8 ~ App ~ apiData", apiData)
+
+  useEffect(() => {
+
+    fetch("http://localhost:5000/sql/all")
+      .then(response => response.json())
+      .then(json => {
+        setApiData(json);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, [])
+
+
+  return (
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Account Name</th>
+          <th>Amount</th>
+          <th>Code</th>
+          <th>Credit</th>
+          <th>Debit</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {apiData ? apiData.map((obj, key) => {
+          var today = new Date();
+          var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+          var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();;
+          var dateTime = date + ' ' + time;
+          return (
+            <>
+              <tr colSpan={2} key={key}>
+                <td>{key}</td>
+                <td>{dateTime}</td>
+                <td>{obj.Debit_Data}</td>
+                <td>{obj.Credit_Data_Amount_Type_Entry}</td>
+                <td>{obj.Debit_Info_Nature == "credit" ? "credit" : null}</td>
+                <td>{obj.Debit_Info_Nature == "Debit" ? "Debit" : null}</td>
+                <td>{obj.Debit_Info_Amount}</td>
+              </tr>
+              <tr key={key + 1}>
+                <td>{key}</td>
+                <td>{dateTime}</td>
+                <td>{obj.Credit_Data}</td>
+                <td>{obj.Credit_Data_Amount_Type_Entry}</td>
+                <td>{obj.Credit_Data_Amount_Nature == "Credit" ? "Credit" : null}</td>
+                <td>{obj.Credit_Data_Amount_Nature == "Debit" ? "Debit" : null}</td>
+                <td>{obj.Credit_Data_Amount}</td>
+              </tr>
+            </>
+
+          )
+        })
+          :
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>}
+      </tbody>
+    </Table>
+  );
+}
+
+export default DarkExample;
--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,47 +1,14 @@
 import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Table from '../component/table'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [apiData, setApiData] = useState([]);
-  console.log("🚀 ~ file: App.jsx ~ line 8 ~ App ~ apiData", apiData)
-
-  useEffect(() => {
-
-    fetch("http://localhost:5000/sql/all")
-        .then(response => response.json())
-        .then(json => {
-            setApiData(json);
-        })
-        .catch(err => {
-            console.error(err);
-        })
-}, [])
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+   <>
+   <Table/>
+   </>
   )
 }
 
